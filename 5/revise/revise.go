24f8de6ef284bc537895f9a80dc67ea925f63052
王林 <576101059@qq.com>
2022-04-01 17:33:54 +0800

    added

---
+++ 5/revise/revise.go
@@ -0,0 +1,16 @@
+/**
+ * @Author: wanglin
+ * @Author: wanglin@vspn.com
+ * @Date: 2022/4/1 17:05
+ * @Desc: TODO
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+
+	fmt.Println(int64(num) + 0xFF000000)
+}
---
+++ 5/trap/trap.go
@@ -0,0 +1,16 @@
+/**
+ * @Author: wanglin
+ * @Author: wanglin@vspn.com
+ * @Date: 2022/4/1 16:54
+ * @Desc: TODO
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+
+	fmt.Println(num + 0xFF000000)
+}
