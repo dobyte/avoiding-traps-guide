1a77b6ffb3bf20ab90b9c7d8212bdb1f08971cd2
fuxiao <576101059@qq.com>
2022-04-13 15:50:43 +0800

    added

---
+++ 6/trap/trap.go
@@ -0,0 +1,58 @@
+/**
+ * @Author: fuxiao
+ * @Email: 576101059@qq.com
+ * @Date: 2022/4/13 11:29 上午
+ * @Desc: TODO
+ */
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"runtime/pprof"
+	"time"
+)
+
+func callWithTimeout(f func() error, timeout time.Duration) (err error) {
+	ch := make(chan error)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	go func() {
+		ch <- f()
+	}()
+
+	select {
+	case err = <-ch:
+	case <-ctx.Done():
+	}
+
+	return
+}
+
+func servePProf() {
+	addr := ":8080"
+
+	fmt.Printf("pporf server started, listening on %s\n", addr)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		p := pprof.Lookup("goroutine")
+		_ = p.WriteTo(w, 1)
+	})
+	_ = http.ListenAndServe(addr, nil)
+}
+
+func main() {
+	go servePProf()
+
+	_ = callWithTimeout(func() error {
+		time.Sleep(5 * time.Second)
+		return errors.New("remote call failed")
+	}, time.Second)
+
+	select {}
+}
